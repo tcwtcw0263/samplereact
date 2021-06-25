6e9e08024a3f0885b654ab8e1445af43c52871ef
tcwtcw0263 <86482358+tcwtcw0263@users.noreply.github.com>
2021-06-25 20:39:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Page1 from './components/Page1';
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
 // import { Provider } from 'react-redux';
-// commment login page
+// commment login page welcome vishnu
 import Provider from 'react-redux/es/components/Provider';
 import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
 import store from '../src/components/store'
